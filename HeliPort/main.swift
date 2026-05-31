a602b0294fc1c34345cb6054ff522e274b6a4097
Emilio P Egido <eaperez@me.com>
2026-05-31 16:02:16 +0200

    Add main.swift and remove MainMenu.xib
    Remove old MainMenu.xib:
    - Switch app entry to a programmatic main: add main.swift that instantiates NSApplication and AppDelegate and calls run()
    - Remove MainMenu.xib and the NSMainNibFile key from Info.plist, and delete the @NSApplicationMain attribute from AppDelegate.swift.

---
+++ HeliPort/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  HeliPort
+//
+//  Created by 梁怀宇 on 2020/3/20.
+//  Copyright © 2020 OpenIntelWireless. All rights reserved.
+//
+
+/*
+ * This program and the accompanying materials are licensed and made available
+ * under the terms and conditions of the The 3-Clause BSD License
+ * which accompanies this distribution. The full text of the license may be found at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import Cocoa
+
+private let app = NSApplication.shared
+private let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
